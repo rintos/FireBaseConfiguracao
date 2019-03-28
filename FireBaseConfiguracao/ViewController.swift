f50e5bdc0354de2e6eedb8189b21637e034bc85b
VictorSantoryu <victor.santoryu@github.com>
2019-03-27 22:57:52 -0300

    Recuperando dados do noh firebase

--- FireBaseConfiguracao/ViewController.swift
+++ FireBaseConfiguracao/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var pontosLabel:UILabel!
+    
+    let fireBaseReference = Database.database().reference()
+    //reference(withPath: "" )
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("rodando app firebase")
+        
+       
+        let pacoteUser = fireBaseReference.child("pontuacao").child("valor")
+        //remover noh
+        //pacoteUser.removeValue()
+        
+        //Recupera dados do FireBase
+        pacoteUser.observe(DataEventType.value) { (dados) in
+            
+            let pontos = dados.value as! String
+            self.pontosLabel.text = pontos
+        }
     }
 
 
